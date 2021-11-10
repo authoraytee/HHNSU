bb0944b9a5984424cfc57429db9cd2ef320656e0
authoraytee <authoraytee@github.com>
2021-11-11 05:44:22 +0700

    f

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from config import bot
+import telebot
 
-from modules.echo import echo
+TOKEN = '2036020508:AAFP8dlyx70n_oW7M-MZ8w9uOa6dZl8KpuQ'
 
-if __name__ == '__main__':
-    echo(bot)
-    bot.polling(none_stop=True)
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(content_types=['text'])
+def simple_echo(message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.polling(none_stop=True)
